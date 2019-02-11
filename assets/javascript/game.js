fbdbbc0da43631457c8d602318b616982480f4d6
Benham <Benham@Brets-MacBook-Air.local>
2019-02-10 21:32:43 -0500

    final updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,10 +11,10 @@ var losses = 0;
 var counter = 0;
 
 var gemImage = [4];
-gemImage[0] = "assets/images/crystal1.jpeg";
-gemImage[1] = "assets/images/crystal2.jpg";
-gemImage[2] = "assets/images/crystal3.jpeg";
-gemImage[3] = "assets/images/crystal4.jpeg";
+gemImage[0] = "assets/images/gem1.png";
+gemImage[1] = "assets/images/gem2.png";
+gemImage[2] = "assets/images/gem3.png";
+gemImage[3] = "assets/images/gem4.png";
 
 // Start
 var resetFunction = function () {
